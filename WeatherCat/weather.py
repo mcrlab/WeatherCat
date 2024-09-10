554a82cc0ad40519e4d15d2a30b86e700fa73c83
James Linnegar <james@linnegar.com>
2024-09-10 21:48:53 +0100

    unused

--- WeatherCat/weather.py
+++ WeatherCat/weather.py
@@ -2,8 +2,7 @@ import os
 import requests
 import urllib.parse as parse
 from dotenv import load_dotenv
-import time
-import json
+
 
 load_dotenv()
 HOST = os.environ.get("HOST")
@@ -21,8 +20,6 @@ class WeatherService():
             data = a.json()
             series = data['results'][0]['series'][0]['columns']
             forecastData = data['results'][0]['series'][0]['values']
-
-            print(series)
             forecasts = []
             for forecast in forecastData:
                 forecastObject = {}
@@ -37,50 +34,6 @@ class WeatherService():
         
         except Exception as e:
             print(e)
-
-    def fetch_weather_old(self):
-        try:
-            if ENV == 'development!':
-                print('loading dev data')
-                f = open("./sample.json", "r");
-                d = f.read()
-                f.close()
-                data = json.loads(d)
-            else:
-                query = """ SELECT temperature, pressure, description, precipitation, humidity, "wind description" from weather where time > now() limit 12"""
-                a = requests.get("http://{0}:8086/query?prety=true&db={1}&q={2}".format(HOST, DB, parse.quote(query)))
-                data = a.json()
-            print(data)
-            forecasts = []
-            for i in range(0,11):
-                forecast_time =         data['results'][0]['series'][0]['values'][i][0]
-                temperature =           data['results'][0]['series'][0]['values'][i][1]
-                pressure =              data['results'][0]['series'][0]['values'][i][2]
-                weather_description =   data['results'][0]['series'][0]['values'][i][3]
-                precipitation =         data['results'][0]['series'][0]['values'][i][4]
-                humidity =              data['results'][0]['series'][0]['values'][i][5]
-                wind_description =      data['results'][0]['series'][0]['values'][i][6]
-                
-                forecasts.append(WeatherForecast(forecast_time, temperature, weather_description, pressure, precipitation, humidity, wind_description))
-            print(len(forecasts))
-            return forecasts
-        
-        except Exception as e:
-            print(e)
-            return WeatherForecast("", -1, "clear sky", -1)
-        
-class WeatherForecast(object):
-    def __init__(self, time, temperature, description, pressure, precipitation, humidty, wind_description):
-        self.time = time
-        self.temperature = temperature
-        self.description = description
-        self.pressure = pressure
-        self.precipitation = precipitation
-        self.humidity = humidty
-        self.wind_description = wind_description
-
-    def serialize(self):
-        return self.__dict__
     
 class WeatherNotLoadedException(Exception):
     """Weather not loaded"""
